3eccc7f1a6fe8e8e3c340f74941bffba0fdb76aa
priyank <priyankgandhi@gmail.com>
2016-09-21 18:32:46 -0700

    test

--- ai-engine/src/main/java/controller/IndexController.java
+++ ai-engine/src/main/java/controller/IndexController.java
@@ -25,56 +25,56 @@ import ai.api.model.AIResponse;
 
 import com.google.gson.Gson;
 
-
 @Controller
 public class IndexController extends BaseController {
-	
+
 	@Autowired
 	GeneralService generalService;
-	
+
 	@Autowired
 	AIUtil aiUtil;
-	
+
 	@RequestMapping("/")
-	public String index(ModelMap model, Principal principal){	   
-//	    AuthUser authUser = getAuthUser();	    
-		model.addAttribute("message","hello world");
+	public String index(ModelMap model, Principal principal) {
+		// AuthUser authUser = getAuthUser();
+		model.addAttribute("message", "hello world");
 		return "index";
 	}
-	
-	
-	@RequestMapping(value="/webhook", method = { RequestMethod.POST, RequestMethod.GET }, produces={"application/json"})
-    public @ResponseBody WebhookResponse webhook(HttpServletRequest request, HttpServletResponse response) {
-      StringBuffer jb = new StringBuffer();
-      WebhookResponse wr = new WebhookResponse();
-      String line = null;
-      System.out.println("Hi there");
-      try {
-        BufferedReader reader = request.getReader();
-        while ((line = reader.readLine()) != null){
-            jb.append(line);
-        }
-        WebhookRequest obj = aiUtil.parseAIRequest(jb.toString());
-        String action = obj.getResult().getAction();
-          System.out.println("action - " + action );
-          wr.setSpeech("speech -Response from my AI Server");
-          wr.setDisplayText("DisplayText - Response from my AI server");
-          Map<String, Object> data = new HashMap<String, Object>();
-//          data.put("facebook", "this is facebook data from my ai server");          
-          
-          List<Map<String, Object>> contextList = new ArrayList<Map<String,Object>>();
-//          Map<String, Object> contextMap = new HashMap<String, Object>();
-//          contextMap.put("name","aiengine");          
-//          contextMap.put("lifespan",2);     
-//          contextList.add(contextMap);          
-          data.put("facebook", "this is facebook data from my ai server");
-          wr.setContextOut(contextList);
-//          wr.setData(data);
-          wr.setSource("facebooker");
-      } catch (Exception e) {
-    	  System.err.println(e); 
-      }     
-      return wr;
-    }
+
+	@RequestMapping(value = "/webhook", method = { RequestMethod.POST, RequestMethod.GET }, produces = {
+			"application/json" })
+	public @ResponseBody WebhookResponse webhook(HttpServletRequest request, HttpServletResponse response) {
+		StringBuffer jb = new StringBuffer();
+		WebhookResponse wr = new WebhookResponse();
+		String line = null;
+		System.out.println("Hi there");
+		try {
+			BufferedReader reader = request.getReader();
+			while ((line = reader.readLine()) != null) {
+				jb.append(line);
+			}
+			System.out.println(jb.toString());
+			WebhookRequest obj = aiUtil.parseAIRequest(jb.toString());
+			String action = obj.getResult().getAction();
+			System.out.println("action - " + action);
+			wr.setSpeech("speech -Response from my AI Server");
+			wr.setDisplayText("DisplayText - Response from my AI server");
+			Map<String, Object> data = new HashMap<String, Object>();
+			// data.put("facebook", "this is facebook data from my ai server");
+
+			List<Map<String, Object>> contextList = new ArrayList<Map<String, Object>>();
+			// Map<String, Object> contextMap = new HashMap<String, Object>();
+			// contextMap.put("name","aiengine");
+			// contextMap.put("lifespan",2);
+			// contextList.add(contextMap);
+			data.put("facebook", "this is facebook data from my ai server");
+			wr.setContextOut(contextList);
+			// wr.setData(data);
+			wr.setSource("facebooker");
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+		return wr;
+	}
 
 }
--- ai-engine/src/test/java/util/AIUtilTest.java
+++ ai-engine/src/test/java/util/AIUtilTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ai.api.AIServiceException;
 
 import pojo.AuthUser;
+import pojo.WebhookRequest;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="classpath:config/test-applicationContext.xml")
@@ -29,8 +30,9 @@ public class AIUtilTest {
 	
 	@Test
     public void testParseAIRequest() {
-		String json = "{  \"id\": \"8efed4e9-6d3d-4868-98f2-2cacdc211a0e\",  \"timestamp\": \"2016-09-22T00:36:24.242Z\",  \"result\": {    \"source\": \"agent\",    \"resolvedQuery\": \"lounge\",    \"speech\": \"\",    \"action\": \"\",    \"actionIncomplete\": false,    \"parameters\": {      \"Lounge-Access\": \"Lounge\"    },    \"contexts\": [],    \"metadata\": {      \"intentId\": \"d749bf4c-7c8f-4b4a-9017-bdb46029c2bf\",      \"webhookUsed\": \"true\",      \"intentName\": \"Give me my lounge information\"    },    \"fulfillment\": {      \"speech\": \"\"    },    \"score\": 0.86  },  \"status\": {    \"code\": 200,    \"errorType\": \"success\"  },  \"sessionId\": \"e2b74f5c-55ff-455b-a980-35f3ac6b30e2\"}";
-		aiUtil.parseAIRequest(json);
+		String json = "{  \"id\": \"8efed4e9-6d3d-4868-98f2-2cacdc211a0e\",  \"timestamp\": \"2016-09-22T00:36:24.242Z\",  \"result\": {    \"source\": \"agent\",    \"resolvedQuery\": \"lounge\",    \"speech\": \"\",    \"action\": \"loungerfinder\",    \"actionIncomplete\": false,    \"parameters\": {      \"Lounge-Access\": \"Lounge\"    },    \"contexts\": [],    \"metadata\": {      \"intentId\": \"d749bf4c-7c8f-4b4a-9017-bdb46029c2bf\",      \"webhookUsed\": \"true\",      \"intentName\": \"Give me my lounge information\"    },    \"fulfillment\": {      \"speech\": \"\"    },    \"score\": 0.86  },  \"status\": {    \"code\": 200,    \"errorType\": \"success\"  },  \"sessionId\": \"e2b74f5c-55ff-455b-a980-35f3ac6b30e2\"}";
+		WebhookRequest res = aiUtil.parseAIRequest(json);
+		System.out.println(res.getResult().getAction());
     }	
 
 }

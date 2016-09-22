99889ae6da323fe783908c9a1b0a17702784366b
priyank <priyankgandhi@gmail.com>
2016-09-21 22:00:21 -0700

    key builder

--- ai-engine/src/main/java/service/GeneralService.java
+++ ai-engine/src/main/java/service/GeneralService.java
@@ -6,6 +6,7 @@ package service;
 import java.util.HashMap;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -41,14 +42,22 @@ public class GeneralService {
 	}	
 	
 	public String intentKeyBuilder(String intentName, HashMap<String, JsonElement> params) {
-		String ret = "";
-		String city = params.get("city").getAsString();
-		String geocity = params.get("geo-city").getAsString();
-		String airport = params.get("airport_code").getAsString();
-		String merchant = params.get("merchant").getAsString();
+		String ret = "";		
+		CustomData customData = customDataRepository.findByKey("keylist");
+		String keys = customData.getValue();
+		System.out.println("Key List - " + keys);
+		
+		String[] keysArray = keys.split(":");
 		StringBuffer sb = new StringBuffer();
-		sb.append(intentName+"-").append(city).append(geocity).append(airport).append(merchant);			
+		sb.append(intentName);		
+		for (String key : keysArray) {
+			String val = params.get(key) != null ? params.get(key).getAsString() : "";
+			if(StringUtils.isNotEmpty(val)) {
+				sb.append("-").append(val);	
+			}			
+		}
 		ret = sb.toString().toLowerCase();
+		System.out.println("Intent Key - " + ret);
 		return ret;
 	}
 }
